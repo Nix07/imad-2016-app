cf79ccf9e7f7667f6b4096ff3449dabb3051f9db
Nix07 <nikhil07prakash@gmail.com>
2016-09-19 19:58:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ app.get('/article-one',function (req,res){
 });
 
 app.get('/article-two',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-three',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    res.send("Hello article three!");
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
