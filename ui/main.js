e3a58eefb813b68d43aa7501f35e8fac7f2d260c
Nix07 <nikhil07prakash@gmail.com>
2016-09-23 20:13:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 
 //Executing new javascript
-    var elem = document.getElementById('main');
+var elem = document.getElementById('main');
 elem.innerHTML = "New Value";
+
+//Moving the madi
+var elem2 = document.getElementById('madi');
+elem2.onclick = function () {
+   elem2.style.marginLeft = 100px;  
+};
