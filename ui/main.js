b2847b2a9a865926f0eb0f0174a26855dfebc084
Nix07 <nikhil07prakash@gmail.com>
2016-09-25 17:18:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,8 @@
 //Counter code
-var button = document.get ElementById("button");
-button.onclick() = function(){
+var button = document.getElementById("button");
+var counter = 0;
+
+button.onclick = function(){
     counter = counter +1;
     var span = document.getElementbyId("count");
     span.innerHTML = counter.toString();
