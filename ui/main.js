827d5dd30006e308879fa1d49596b0b3d3bcb5fc
Nix07 <nikhil07prakash@gmail.com>
2016-09-25 21:19:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,8 +21,6 @@ button.onclick = function () {
 
 
 //Response from the server
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     //Make the request to the server and send the name
@@ -43,7 +41,8 @@ submit.onclick = function() {
             }
        }
     };
-    
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://nix07.imad.hasura-app.io/submit-name?name' + name, true);
         request.send(null);
     
