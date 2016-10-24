14f13a2de661a2cdb00548ef77567f8383c4c4d1
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-10-24 18:57:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,9 +105,22 @@ app.get('/submit-name', function(req, res) {
 });
 
 
-app.get('/:articleName',function (req,res){
-    var articleName = req.params.articleName;
-   res.send(createtemplate(articles[articleName]));
+app.get('/articles/:articleName',function (req,res){
+   // var articleName = req.params.articleName;
+    
+    pool.query("SELECT * FROM article WHERE title =" + req.params.articleName, function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.length === 0 ){
+                res.status(404).send('Article not found');
+            }
+            else{
+                var articleData = result.rows[0];
+                res.send(createtemplate(articleData));
+            }
+        }
+    });
 });
 
 app.get('/ui/bootstrap.css', function (req, res) {
