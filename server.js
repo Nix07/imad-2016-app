cc7901cfa739afdd1ad9ebdb020ce9643257d962
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-11-01 01:44:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,8 +79,8 @@ function createtemplate2(data){
 				<ul class="nav navbar-nav navbar-right">
 					<li><a href="/">Home</a></li>
 					<li><a href="about.html">About us</a></li>
-					<li><a href="index3.html">Posts</a></li>
-					<li><a href="index4.html">Gallery</a></li>
+					<li><a href="/index3.html">Posts</a></li>
+					<li><a href="/index4.html">Gallery</a></li>
 				</ul>
 			</div>
 		</nav>
