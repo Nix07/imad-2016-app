a78a47f282ea3f7ea364f5b1dbd1d63e279fb28f
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-11-01 00:35:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.get('/articles/:articleName',function (req,res){
             }
             else{
                 var articleData = result.rows[0];
-                res.send(createtemplate2(articleData));
+                res.send(createtemplate1(articleData));
             }
         }
     });
