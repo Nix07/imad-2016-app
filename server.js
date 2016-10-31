53d8269b220a8c6b77786c1b51bc6758b9ce7d34
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-11-01 00:53:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function createtemplate2(data){
             		<title>My Blog</title>
             		<link rel="stylesheet" href="/ui/css/bootstrap.css">
             		<link rel="stylesheet" href="css/style2.css">
-            		<script src="js/jquery.js'></script>
+            		<script src="js/jquery.js"></script>
             		<script src="js/bootstrap.js"></script>
             	</head>
             
