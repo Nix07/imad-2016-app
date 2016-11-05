182dab7bed447ee5d575edf121eccd53454694e8
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-11-06 01:38:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ function createtemplate2(data){
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index2.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var pool = new Pool(config);
