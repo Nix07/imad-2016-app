d0ee8d5656467895efcd0d0788d8ca47ff6ccb0a
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-11-10 17:25:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/hash/:input', function(req, res) {
    res.send(hashedString);
 });
 
-app.post('/create-user', function (req, res) {
+\app.post('/create-user', function (req, res) {
    // username, password
    // {"username": "tanmai", "password": "password"}
    // JSON
