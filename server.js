569df8d5948be945795109dbb345a2be61b22c02
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-11-11 23:40:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,14 +118,16 @@ function createtemplate2(data){
 				</div>
 				
 			</div>
-			<h4>Comments</h4>
-              <div id="comment_form">
+		</div>
+		<div class="row">
+    		<h4>Comments</h4>
+                  <div id="comment_form">
+                  </div>
+    			<div id="comments">
+                    <center>Loading comments...</center>
+                 </div>
               </div>
-			<div id="comments">
-                <center>Loading comments...</center>
-             </div>
-          </div>
-          <script type="text/javascript" src="/ui/article.js"></script>
+             <script type="text/javascript" src="/ui/article.js"></script>
 		</div>
 		<div class="container-fluid">
 			<hr>
