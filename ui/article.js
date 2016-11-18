2c209f074d3b249f6424eef6a255cc32efdce923
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-11-18 18:19:05 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -3,7 +3,7 @@ var currentArticleTitle = window.location.pathname.split('/')[2];
 
 function loadCommentForm () {
     var commentFormHtml = `
-        <div class="col-md-12">
+        <div class="row">
 			<div class="col-md-12">
 				<div class="well">
 					<div class="text-right">
