198749f427c19b1643267fd4830f628f25a2bdb6
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-11-18 17:51:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,9 @@
 
 function loadLoginForm () {
     var loginHtml = `
-    <style>
-        .mobile {
-            width: 30%;
-            margin: 25px auto;
-        }
-
-        @media (max-width: 600px) {
-            .mobile {
-                width: 80%;
-                 margin: 25px auto;
-        }
-    </style>
-    <div class="container" style="margin-top: 70px;">
-      <div class="row">
-          <h1 style="text-align: center;">LOGIN</h1>
-          <div class="mobile">
-              <form>
-                  <div class="form-group">
+    <div class="container-fluid col-md-6 col-md-offset-3 text-center">
+		  <form style="padding-right: 20px; padding-left=20px;">
+			    <div class="form-group">
 			      <label for="email">Username:</label>
 			      <input type="text" class="form-control" id="username" placeholder="Username">
 			    </div>
@@ -26,11 +11,12 @@ function loadLoginForm () {
 			      <label for="pwd">Password:</label>
 			      <input type="password" class="form-control" id="password" placeholder="Enter password">
 			    </div>
+			    
+			    <button type="submit" class="btn btn-default btn-left" id="login_btn" style="padding= 50px;">Login</button>
 			    <button type="submit" class="btn btn-default" id="register_btn">Register</button>
-              </form>
-          </div>
-      </div>
-    </div>
+		  </form>
+		   </div>
+		   <br>
         `;
     document.getElementById('login_area').innerHTML = loginHtml;
     
