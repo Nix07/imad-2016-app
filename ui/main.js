744ef2249d204ce9cc3e6273d8d2c2df71de9497
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-11-18 16:46:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,39 @@
 
 function loadLoginForm () {
     var loginHtml = `
-    <h1 class="text-center">Login/Register</h1>
-        <div class="container-fluid col-md-6 col-md-offset-3 text-center">
-		  <form style="padding-right: 20px; padding-left=20px;">
-		    <div class="form-group">
-		      <label for="email">Username:</label>
-		      <input type="text" class="form-control" id="username" placeholder="Username">
-		    </div>
-		    <div class="form-group">
-		      <label for="pwd">Password:</label>
-		      <input type="password" class="form-control" id="password" placeholder="Enter password">
-		    </div>
-		    <button type="submit" class="btn btn-default btn-left" id="login_btn" style="padding= 50px;">Login</button>
-		    <button type="submit" class="btn btn-default" id="register_btn">Register</button>
-		  </form>
-		   </div>
-		   <br>
-		   
     
+    <style>
+        .mobile {
+            width: 30%;
+            margin: 25px auto;
+        }
+
+        @media (max-width: 600px) {
+            .mobile {
+                width: 80%;
+                 margin: 25px auto;
+        }
+    </style>
+    <div class="container" style="margin-top: 70px;">
+        <div class="row">
+            <h1 style="text-align: center">Create New Account!</h1>
+                <div class="mobile">
+		            <form style="padding-right: 20px; padding-left=20px;">
+		                <div class="form-group">
+		                    <label for="email">Username:</label>
+		                    <input type="text" class="form-control" id="username" placeholder="Username">
+		                </div>
+		                <div class="form-group">
+		                    <label for="pwd">Password:</label>
+		                    <input type="password" class="form-control" id="password" placeholder="Enter password">
+		                </div>
+		                <button type="submit" class="btn btn-default btn-left" id="login_btn" style="padding= 50px;">Login</button>
+		                <button type="submit" class="btn btn-default" id="register_btn">Register</button>
+		            </form>
+		        </div>
+		   <br>
+		</div>  
+    </div>
         `;
     document.getElementById('login_area').innerHTML = loginHtml;
     
