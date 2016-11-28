c8fd23c0badfbb90edd261ad9eba32a70e735986
Nikhil Prakash <nikhil07prakash@gmail.com>
2016-11-28 18:37:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -178,7 +178,7 @@ app.get('/hash/:input', function(req, res) {
    res.send(hashedString);
 });
 
-app.post('/create-user', function (req, res) {
+/*app.post('/create-user', function (req, res) {
    // username, password
    // {"username": "tanmai", "password": "password"}
    // JSON
@@ -193,12 +193,29 @@ app.post('/create-user', function (req, res) {
           res.send('User successfully created: ' + username);
       }
    });
-});
+});*/
+
+app.post('/create-user', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  if(!username.trim() || !password.trim()){
+      res.status(400).send('Username or password field blank.');   //Err if blank,tabs and space detected.
+   }else{
+        var salt = crypto.randomBytes(128).toString('hex');
+        var dbString = hash(password, salt);
+        pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString],         function (err, result) {
+    if(err) {
+       res.status(500).send(err.toString());
+    } else {
+       res.send('User successfully created: ' + username);
+    }
+  });
+ }
 
 app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
-    if (username==='' || password==='') {
+    /*if (username==='' || password==='') {
         alert("Field can't be empty!");
         console.log('yes');
         return;
@@ -206,36 +223,38 @@ app.post('/login', function (req, res) {
     
     if (username.length===0 || password.length===0){
         alert("Field can't be empty!");
-        console.log('yes3');
         return;
-    }
-   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          if (result.rows.length === 0) {
-              res.status(403).send('username/password is invalid');
+    }*/
+    if(!username.trim() || !password.trim()){
+        res.status(400).send('Username or password field blank.');   //Err if blank,tabs and space detected.
+    } else 
+        pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+          if (err) {
+              res.status(500).send(err.toString());
           } else {
-              // Match the password
-              var dbString = result.rows[0].password;
-              var salt = dbString.split('$')[2];
-              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
-              if (hashedPassword === dbString) {
-                
-                // Set the session
-                req.session.auth = {userId: result.rows[0].id};
-                // set cookie with a session id
-                // internally, on the server side, it maps the session id to an object
-                // { auth: {userId }}
-                
-                res.send('credentials correct!');
-                
+              if (result.rows.length === 0) {
+                  res.status(403).send('username/password is invalid');
               } else {
-                res.status(403).send('username/password is invalid');
+                  // Match the password
+                  var dbString = result.rows[0].password;
+                  var salt = dbString.split('$')[2];
+                  var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+                  if (hashedPassword === dbString) {
+                    
+                    // Set the session
+                    req.session.auth = {userId: result.rows[0].id};
+                    // set cookie with a session id
+                    // internally, on the server side, it maps the session id to an object
+                    // { auth: {userId }}
+                    
+                    res.send('credentials correct!');
+                    
+                  } else {
+                    res.status(403).send('username/password is invalid');
+                  }
               }
           }
-      }
-   });
+       });
 });
 
 app.get('/check-login', function (req, res) {
